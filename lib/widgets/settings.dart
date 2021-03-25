370362ee0fcb2ea9b60fb8ef7fba9db40d8d01ff
msd999 <msd9programmer@gmail.com>
2021-03-25 18:37:04 +0200

    up

---
+++ lib/widgets/settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  _Settings createState() => _Settings();
+}
+
+class _Settings extends State<Settings> {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
