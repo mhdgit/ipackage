5a3a9357e4fcd54d5450691a8dcb7c3c8a8d4b72
msd999 <msd9programmer@gmail.com>
2021-04-03 20:19:31 +0300

    up

---
+++ lib/widgets/users/new_account.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class New_account extends StatefulWidget {
+  @override
+  _New_account createState() => _New_account();
+}
+
+class _New_account extends State<New_account> {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
